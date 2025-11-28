a3a40846d0e84ffbe72dae2a39c1be9e7d0960b8
Luc Fery <Lucasus822@gmail.com>
2025-11-28 16:36:11 +0100

    Feature contact count

--- src/components/contactlist/Contactlist.vue
+++ src/components/contactlist/Contactlist.vue
@@ -3,42 +3,42 @@ import ContactlistHeader from './ui/ContactlistHeader.vue';
 import SearchBar from './ui/SearchBar.vue';
 import ListTable from './ui/listtable/ListTable.vue';
 import DB from '@/DB';
-import { reactive, onMounted, watch } from 'vue';
+import { computed, onMounted, watch, ref } from 'vue';
 
-const contacts = reactive([]);
-
+const contacts = ref([]);
+const contactCount = ref(0);
 onMounted(async () => {
   DB.setApiUrl('https://691b0e532d8d7855757146d3.mockapi.io/');
   const response = await DB.findAll();
-  contacts.splice(contacts.length, 0, ...response);
+  contacts.value.splice(contacts.value.length, 0, ...response);
   console.table(contacts);
 });
 
 const props = defineProps({
   formData: { type: Object },
 });
-
 const deleteContact = async (id) => {
   await DB.deleteOneById(id);
-  const index = contacts.findIndex((c) => c.id === id);
+  const index = contacts.value.findIndex((c) => c.id === id);
   if (index != -1) {
-    contacts.splice(index, 1);
+    contacts.value.splice(index, 1);
   }
 };
 const addContact = async () => {
   const response = await DB.create(props.formData);
-  contacts.splice(contacts.length, 0, response);
+  contacts.value.splice(contacts.value.length, 0, response);
   console.table(contacts);
 };
 const updateContact = async (newContact) => {
   await DB.updateOne(newContact);
 };
+const getContactCount = computed(() => contacts.value.length);
 watch(props.formData, addContact);
 </script>
 <template>
   <!-- Section droite pour la liste des contacts -->
   <section class="w-2/3 p-6">
-    <contactlist-header />
+    <contactlist-header :contactCount="getContactCount" />
     <!-- Filtre de recherche -->
     <search-bar />
     <!-- Liste des contacts triée et filtrée -->
--- src/components/contactlist/ui/ContactlistHeader.vue
+++ src/components/contactlist/ui/ContactlistHeader.vue
@@ -1,9 +1,13 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  contactCount: { type: Number },
+});
+</script>
 <template>
   <div class="flex justify-between items-center mb-4">
     <h2 class="text-xl font-bold mb-4">Contacts List</h2>
     <p class="text-gray-600 mb-4">
-      Contacts Count : <span class="font-bold">xxx</span>
+      Contacts Count : <span class="font-bold">{{ contactCount }}</span>
     </p>
   </div>
 </template>
